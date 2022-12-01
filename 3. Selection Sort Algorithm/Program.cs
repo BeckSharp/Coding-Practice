e78776fc083c361a56e0de154c3e8ceecdef88a5
cmptbeck <T.Beck@2019.ljmu.ac.uk>
2022-12-01 17:08:50 +0000

    Selection Sort Initial Commit

---
+++ 3. Selection Sort Algorithm/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SelectionSortAlgorithm // Note: actual namespace depends on the project name.
+{
+    internal class SelectionSortAlgorithm
+    {
+        static void Main(string[] args)
+        {
+            int[] data = {7, 8, 2, 3, 5, 1, 0, -10};
+            
+            data = selectionSort(data);
+
+            printResults(data);
+        }
+
+        static int[] selectionSort(int[] data) 
+        {
+
+            return data;
+        }
+        
+        static void printResults(int[] data) 
+        {
+            Console.WriteLine("[{0}]", string.Join(", ", data));
+        }    
+    }
+}

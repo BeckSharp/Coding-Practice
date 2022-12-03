7d95c84261a42d2b4b4b3615ef9066db7ef3bd5e
cmptbeck <T.Beck@2019.ljmu.ac.uk>
2022-12-03 15:48:46 +0000

    Quick Sort Initial Commit

---
+++ 5. Quick Sort Algorithm/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace QuickSortAlgorithm
+{
+    internal class QuickSortAlgorithm
+    {
+        static void Main(string[] args)
+        {
+            int[] data = {7, 8, 2, 3, 5, 1, 0, -10};
+            
+            data = quickSort(data);
+
+            printResults(data);
+        }
+
+        static int[] quickSort(int[] data) 
+        {
+
+            return data;
+        }
+        
+        static void printResults(int[] data) 
+        {
+            Console.WriteLine("[{0}]", string.Join(", ", data));
+        }    
+    }
+}

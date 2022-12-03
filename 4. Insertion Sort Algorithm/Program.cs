2467dd36faacbcc0c2796e380288591dccb99230
cmptbeck <T.Beck@2019.ljmu.ac.uk>
2022-12-03 01:53:29 +0000

    Insertion Sort Initial Commit

---
+++ 4. Insertion Sort Algorithm/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace InsertionSortAlgorithm
+{
+    internal class InsertionSortAlgorithm
+    {
+        static void Main(string[] args)
+        {
+            int[] data = {7, 8, 2, 3, 5, 1, 0, -10};
+            
+            data = insertionSort(data);
+
+            printResults(data);
+        }
+
+        static int[] insertionSort(int[] data) 
+        {
+
+            return data;
+        }
+        
+        static void printResults(int[] data) 
+        {
+            Console.WriteLine("[{0}]", string.Join(", ", data));
+        }    
+    }
+}

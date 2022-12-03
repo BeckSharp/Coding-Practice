7c6b9570272378e12940bd32b176f04e709023a4
cmptbeck <T.Beck@2019.ljmu.ac.uk>
2022-12-03 18:31:22 +0000

    Quick Sort Implemented

--- 5. Quick Sort Algorithm/Program.cs
+++ 5. Quick Sort Algorithm/Program.cs
@@ -8,17 +8,48 @@ namespace QuickSortAlgorithm
         {
             int[] data = {7, 8, 2, 3, 5, 1, 0, -10};
             
-            data = quickSort(data);
+            data = quickSort(data, 0, (data.Length - 1));
 
             printResults(data);
         }
 
-        static int[] quickSort(int[] data) 
+        static int[] quickSort(int[] data, int left, int right) 
         {
+            if (left < right)
+            {
+                int pivot = findPivot(data, left, right);
+
+                if (pivot > 1) { return quickSort(data, left, (pivot - 1)); }
+                if ((pivot + 1) < right) { return quickSort(data, (pivot + 1), right); }
+            }
 
             return data;
         }
         
+        static int findPivot(int[] data, int left, int right)
+        {
+            int pivot = data[left];
+
+            while (true)
+            {
+                while (data[left] < pivot) { left++; }
+                while (data[right] > pivot) { right--; }
+
+                if (left < right)
+                {
+                    if (data[left] == data[right]) { return right; }
+                    
+                    int temp = data[left];
+                    data[left] = data[right];
+                    data[right] = temp;
+                }
+                else
+                {
+                    return right;
+                }
+            }
+        }
+
         static void printResults(int[] data) 
         {
             Console.WriteLine("[{0}]", string.Join(", ", data));
